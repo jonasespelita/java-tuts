9238ab38e38704b3e84a0483ce06815817acb16d
Jonas Espelita <jonasespelita@gmail.com>
2018-10-02 02:12:01 +0800

    added content

---
+++ sample-progs/SimpleJavaProgram/src/main/java/espelita/fun/simplejavaprogram/SimpleJavaProgram.java
@@ -0,0 +1,17 @@
+package espelita.fun.simplejavaprogram;
+
+/**
+ * A Simple Java Program
+ * @author Jonas
+ */
+public class SimpleJavaProgram {
+
+    public static void main(String[] args) {
+        String msg = "hello world!";
+        if (!msg.isEmpty()) {
+            System.out.println(msg);
+        } else {
+            System.out.println("no message!");
+        }
+    }//End of main
+}//End of SimpleJavaProgram Class
